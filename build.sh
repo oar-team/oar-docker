c5c778453aad394f643f91f2e4890532267d80ca
Salem Harrache <salem@harrache.info>
2014-03-23 08:54:57 +0100

    Remove intermediate images and containers used for build

--- build.sh
+++ build.sh
@@ -4,11 +4,12 @@ set -o errexit
 BASEDIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 VERSION=$(cat $BASEDIR/version.txt)
 
-docker build -t oarcluster/base $BASEDIR/base/
-docker build -t oarcluster/dnsmasq $BASEDIR/dnsmasq/
+docker build --rm -t oarcluster/base $BASEDIR/base/
+docker build --rm -t oarcluster/dnsmasq $BASEDIR/dnsmasq/
 
 NODES=("frontend" "node" "server")
 for image in "${NODES[@]}"; do
     docker build --rm -t oarcluster/$image:${VERSION} $BASEDIR/$image/
     docker tag oarcluster/$image:${VERSION} oarcluster/$image:latest
 done
+docker images | grep "<none>" | awk '{print $3}' | xargs -I {} docker rmi -f {}
