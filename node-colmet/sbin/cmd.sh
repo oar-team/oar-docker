cde76183ddb261735c5d3a6d781b1e4f20387b8d
Salem Harrache <salem@harrache.info>
2014-04-27 15:41:22 +0200

    Fixed node-colmet init process

---
+++ node-colmet/sbin/cmd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+mkdir -p /var/log/supervisor
+
+touch /var/log/auth.log
+touch /var/log/supervisor/oar-node.log
+touch /var/log/supervisor/colmet-node.log
+/sbin/rtail /var/log/auth.log /var/log/supervisor/oar-node.log /var/log/supervisor/colmet-node.log
