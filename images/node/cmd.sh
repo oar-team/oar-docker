c5ac5038e3cbf8e6f8f870096ed87278aa93949c
Salem Harrache <salem@harrache.info>
2014-05-02 17:59:19 +0200

    Changed debootstrap cache path to make it more persistent

--- images/node/cmd.sh
+++ images/node/cmd.sh
@@ -5,4 +5,6 @@ mkdir -p /var/log/supervisor
 
 touch /var/log/auth.log
 touch /var/log/supervisor/oar-node.log
-/sbin/rtail /var/log/auth.log /var/log/supervisor/oar-node.log
+touch /var/log/auth.log
+
+/sbin/rtail /var/log/auth.log /var/log/supervisor/oar-node.log /var/log/auth.log
