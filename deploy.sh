21d312e2e2e686bca44b29242b1be6b80937f129
Salem Harrache <salem@harrache.info>
2014-05-02 17:32:34 +0200

    Enabled insecure key for colmet node

--- deploy.sh
+++ deploy.sh
@@ -101,7 +101,7 @@ start_nodes() {
 }
 
 start_nodes_colmet() {
-    start_nodes "oarcluster/node-colmet:latest" "/sbin/init_kvm /sbin/my_init /sbin/cmd.sh"
+    start_nodes "oarcluster/node-colmet:latest" "/sbin/init_kvm /sbin/my_init /sbin/cmd.sh --enable-insecure-key"
 }
 
 copy_ssh_config() {
