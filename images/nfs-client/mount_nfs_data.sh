d0cbf7946163379f6e977ebc1b999dd23b1eb176
Salem Harrache <salem.harrache@inria.fr>
2014-06-02 17:46:00 +0200

    auto-mount nfs-node

--- images/nfs-client/mount_nfs_data.sh
+++ images/nfs-client/mount_nfs_data.sh
@@ -7,13 +7,13 @@ SRC="/data"
 rpcbind &
 
 
-# echo -n "Waiting for nfs-server to become available"
-# until ssh-keyscan nfs-server | grep ssh-rsa\\\|ssh-dsa &> /dev/null
-# do
-#     sleep 1
-#     echo -n "."
-# done
+echo -n "Waiting for nfs-server to become available"
+until ssh-keyscan nfs-server | grep ssh-rsa\\\|ssh-dsa &> /dev/null
+do
+    sleep 1
+    echo -n "."
+done
 
-# mkdir -p /data
-# echo  "Mount NFS shared folder"
-# mount -t nfs -o rw,user,intr,hard,noatime,rsize=32768,wsize=32768,nfsvers=3 nfs-server:/data /data
+mkdir -p /data
+echo  "Mount NFS shared folder"
+mount -t nfs -o rw,user,intr,hard,noatime,rsize=32768,wsize=32768,nfsvers=3 nfs-server:/data /data
