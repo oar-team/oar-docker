cbfcd20d00f79ee76e08e50bf173648a0ecbab4e
Pierre Neyron <pierre.neyron@imag.fr>
2014-07-21 22:24:28 +0200

    fix init scripts

--- init-scripts/frontend.d/set_ressources.sh
+++ init-scripts/frontend.d/set_ressources.sh
@@ -2,15 +2,19 @@
 set -e
 echo_and_run() { echo "$@" ; $@ ; }
 
-NUM_NODES=3
+while ! oarnodes --sql "false" 2> /dev/null; do
+  sleep 1
+done
+ 
+NUM_NODES=4
 NUM_NODECPU=2
 NUM_CPUCORE=2
 NUM_CPUSET=$(grep -e "^processor\s\+:" /proc/cpuinfo | wc -l)
 core=0
 cpu=0
-for ((i=0;i< $NUM_NODECPU; i++)); do
-  hostname="node${i}"
-  while [ $cpu -lt $((NUM_NODECPU * (i+1))) ]; do
+for ((node=1;node<=$NUM_NODES; node++)); do
+  hostname="node${node}"
+  while [ $cpu -lt $((NUM_NODECPU * node)) ]; do
     while [ $core -lt $((NUM_CPUCORE * (cpu+1))) ]; do
       echo_and_run oarnodesetting -a -h $hostname -p cpu=$cpu -p core=$core -p cpuset=$((core % $NUM_CPUSET))
       core=$((core+1))
--- init-scripts/node.d/add_users.sh
+++ init-scripts/node.d/add_users.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
-set -e
-
-users=( "user1" "user2" "user3" "toto")
-
-
-for name in "${users[@]}"; do
-    echo "Adding user $name..."
-    useradd -m $name -s /bin/bash
-    echo -n "$name:$name" | chpasswd
-    usermod -a -G sudo $name
-done
+../frontend.d/add_users.sh
