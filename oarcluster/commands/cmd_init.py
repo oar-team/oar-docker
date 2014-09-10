a92ea4216d6efc082a2dca375abf0298a9e31c95
Salem Harrache <salem@harrache.info>
2014-09-10 16:47:21 +0200

    ensure that dns hosts file exists

--- oarcluster/commands/cmd_init.py
+++ oarcluster/commands/cmd_init.py
@@ -1,12 +1,14 @@
 import click
 from oarcluster.cli import pass_context
+from oarcluster.utils import touch
 
 
 @click.command('init')
 @click.option('-f', '--force', is_flag=True, help='Overwrite existing env')
 @pass_context
 def cli(ctx, force):
-    """Initializes a new oarcluster environment."""
+    """Initialize a new environment."""
     ctx.copy_tree(ctx.templates_dir, ctx.envdir, force)
+    touch(ctx.dnsfile)
     ctx.log('Initialized oarcluster environment in %s',
             click.format_filename(ctx.envdir))
