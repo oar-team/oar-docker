004e802d657944bb5480e2a19ed0d933f9eff6d2
Salem Harrache <salem@harrache.info>
2014-09-10 16:53:40 +0200

    Renamed postinstall.sh to install_oar.sh

--- oarcluster/templates/images/frontend/install_oar.sh
+++ oarcluster/templates/images/frontend/install_oar.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
 
-WORKDIR=/tmp/postinstall
-SRCDIR=$WORKDIR/src
-GIT_SRC=$WORKDIR/oar-git
+TMPDIR=$(mktemp -d --tmpdir install_oar.XXXXXXXX)
+SRCDIR="$TMPDIR/src"
 
 fail() {
     echo $@ 1>&2
@@ -11,17 +10,23 @@ fail() {
     exit 1
 }
 
-[ -n "$OAR_INSTALL_METHOD" ] || fail "You must set OAR_INSTALL_METHOD variable"
-
 # Create tarball
-if [ $OAR_INSTALL_METHOD = "git" ]; then
-    pushd $GIT_SRC
+if [ -d "$1"  ]; then
+    pushd $1
     TARBALL_PATH=$(make tarball 2>&1 | tail -1)
-    TARBALL=$(readlink -m "$GIT_SRC/$TARBALL_PATH")
+    TARBALL=$(readlink -m "$1/$TARBALL_PATH")
     popd
 else
+    TARBALL=$1
+    [ -n "$TARBALL" ] || fail "error: You must provide a URL to a OAR tarball"
+    if [ ! -r "$TARBALL" ]; then
+        curl $TARBALL -o $TMPDIR/oar-tarball.tar.gz
+        TARBALL=$TMPDIR/oar-tarball.tar.gz
+    else
+        TARBALL="$(readlink -m $TARBALL)"
+    fi
+fi
 
-[ -r "$TARBALL" ] || fail "error: You must provide a OAR tarball"
 VERSION=$(tar xfz $TARBALL --wildcards "*/sources/core/common-libs/lib/OAR/Version.pm" --to-command "grep -e 'my \$OARVersion'" | sed -e 's/^[^"]\+"\(.\+\)";$/\1/')
 [ -n "${VERSION}" ] || fail "error: fail to retrieve OAR version"
 
@@ -33,6 +38,8 @@ make -C $SRCDIR/oar-${VERSION} PREFIX=/usr/local user-build tools-build
 make -C $SRCDIR/oar-${VERSION} PREFIX=/usr/local user-install drawgantt-svg-install monika-install www-conf-install api-install tools-install
 make -C $SRCDIR/oar-${VERSION} PREFIX=/usr/local user-setup drawgantt-svg-setup monika-setup www-conf-setup api-setup tools-setup
 
+rm -rf "$TMPDIR"
+
 # Configure MOTD
 sed -i s/__OAR_VERSION__/${VERSION}/ /etc/motd
 chmod 644 /etc/motd
@@ -43,7 +50,7 @@ a2enmod headers
 a2enmod rewrite
 
 # configure open api
-ln -s /etc/oar/apache2/oar-restful-api-priv.conf /etc/apache2/conf.d/oar-restful-api-priv.conf
+ln -sf /etc/oar/apache2/oar-restful-api-priv.conf /etc/apache2/conf.d/oar-restful-api-priv.conf
 perl -pi -e "s/Deny from all/#Deny from all/" /etc/oar/apache2/oar-restful-api.conf
 
 ## Configure basic auth api
@@ -72,12 +79,12 @@ sed -e "s/^\(username.*\)oar.*/\1oar_ro/" -i /etc/oar/monika.conf
 sed -e "s/^\(password.*\)oar.*/\1oar_ro/" -i /etc/oar/monika.conf
 sed -e "s/^\(dbtype.*\)mysql.*/\1psql/" -i /etc/oar/monika.conf
 sed -e "s/^\(dbport.*\)3306.*/\15432/" -i /etc/oar/monika.conf
-sed -e "s/^\(hostname.*\)localhost.*/\1server/" -i /etc/oar/monika.conf
+sed -e "s/^\(hostname.*\)localhost.*/\1services/" -i /etc/oar/monika.conf
 
 
 # Edit oar.conf
 sed -e 's/^LOG_LEVEL\=\"2\"/LOG_LEVEL\=\"3\"/' -i /etc/oar/oar.conf
-sed -e 's/^DB_HOSTNAME\=.*/DB_HOSTNAME\=\"server\"/' -i /etc/oar/oar.conf
+sed -e 's/^DB_HOSTNAME\=.*/DB_HOSTNAME\=\"services\"/' -i /etc/oar/oar.conf
 sed -e 's/^SERVER_HOSTNAME\=.*/SERVER_HOSTNAME\=\"server\"/' -i /etc/oar/oar.conf
 sed -e 's/^#\(TAKTUK_CMD\=\"\/usr\/bin\/taktuk \-t 30 \-s\".*\)/\1/' -i /etc/oar/oar.conf
 sed -e 's/^#\(PINGCHECKER_TAKTUK_ARG_COMMAND\=\"broadcast exec timeout 5 kill 9 \[ true \]\".*\)/\1/' -i /etc/oar/oar.conf
@@ -93,13 +100,13 @@ sed -e 's/^\(DB_BASE_LOGIN_RO\)=.*/\1="oar_ro"/' -i /etc/oar/oar.conf
 # Configure phppgadmin
 sed -i "s/# allow from all/allow from all/g" /etc/apache2/conf.d/phppgadmin
 sed -i "s/\$conf\['extra_login_security'\] = true;/\$conf\['extra_login_security'\] = false;/g" /etc/phppgadmin/config.inc.php
-sed -i "s/\$conf\['servers'\]\[0\]\['host'\] = 'localhost';/\$conf\['servers'\]\[0\]\['host'\] = 'server';/g" /etc/phppgadmin/config.inc.php
+sed -i "s/\$conf\['servers'\]\[0\]\['host'\] = 'localhost';/\$conf\['servers'\]\[0\]\['host'\] = 'services';/g" /etc/phppgadmin/config.inc.php
 
 ## Visualization tools
 # Configure drawgantt-svg
 sed -i "s/\$CONF\['db_type'\]=\"mysql\"/\$CONF\['db_type'\]=\"pg\"/g" /etc/oar/drawgantt-config.inc.php
-sed -i "s/\$CONF\['db_server'\]=\"127.0.0.1\"/\$CONF\['db_server'\]=\"server\"/g" /etc/oar/drawgantt-config.inc.php
+sed -i "s/\$CONF\['db_server'\]=\"127.0.0.1\"/\$CONF\['db_server'\]=\"services\"/g" /etc/oar/drawgantt-config.inc.php
 sed -i "s/\$CONF\['db_port'\]=\"3306\"/\$CONF\['db_port'\]=\"5432\"/g" /etc/oar/drawgantt-config.inc.php
 sed -i "s/\"My OAR resources\"/\"Docker oarcluster resources\"/g" /etc/oar/drawgantt-config.inc.php
 
-echo "oar-$VERSION"
+echo "$VERSION" | tee /oar_version
--- oarcluster/templates/images/node/install_oar.sh
+++ oarcluster/templates/images/node/install_oar.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
 
-WORKDIR=/tmp/postinstall
-SRCDIR=$WORKDIR/src
-GIT_SRC=$WORKDIR/oar-git
+TMPDIR=$(mktemp -d --tmpdir install_oar.XXXXXXXX)
+SRCDIR="$TMPDIR/src"
 
 fail() {
     echo $@ 1>&2
@@ -11,17 +10,23 @@ fail() {
     exit 1
 }
 
-[ -n "$OAR_INSTALL_METHOD" ] || fail "You must set OAR_INSTALL_METHOD variable"
-
 # Create tarball
-if [ $OAR_INSTALL_METHOD = "git" ]; then
-    pushd $GIT_SRC
+if [ -d "$1"  ]; then
+    pushd $1
     TARBALL_PATH=$(make tarball 2>&1 | tail -1)
-    TARBALL=$(readlink -m "$GIT_SRC/$TARBALL_PATH")
+    TARBALL=$(readlink -m "$1/$TARBALL_PATH")
     popd
 else
+    TARBALL=$1
+    [ -n "$TARBALL" ] || fail "error: You must provide a URL to a OAR tarball"
+    if [ ! -r "$TARBALL" ]; then
+        curl $TARBALL -o $TMPDIR/oar-tarball.tar.gz
+        TARBALL=$TMPDIR/oar-tarball.tar.gz
+    else
+        TARBALL="$(readlink -m $TARBALL)"
+    fi
+fi
 
-[ -r "$TARBALL" ] || fail "error: You must provide a OAR tarball"
 VERSION=$(tar xfz $TARBALL --wildcards "*/sources/core/common-libs/lib/OAR/Version.pm" --to-command "grep -e 'my \$OARVersion'" | sed -e 's/^[^"]\+"\(.\+\)";$/\1/')
 [ -n "${VERSION}" ] || fail "error: fail to retrieve OAR version"
 
@@ -33,4 +38,8 @@ make -C $SRCDIR/oar-${VERSION} PREFIX=/usr/local node-build
 make -C $SRCDIR/oar-${VERSION} PREFIX=/usr/local node-install
 make -C $SRCDIR/oar-${VERSION} PREFIX=/usr/local node-setup
 
-echo "oar-$VERSION"
+rm -rf "$TMPDIR"
+
+echo "Nothing to do to configure oar node !"
+
+echo "$VERSION" | tee /oar_version
