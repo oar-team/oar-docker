8e7eea4728d679ac55a21241ffeff4c024dee149
Pierre Neyron <pierre.neyron@imag.fr>
2014-10-11 13:28:51 +0200

    adapt script to job_resource_manager_cgroup.pl

--- oardocker/templates/my_init.d/available/mount_cpuset.sh
+++ oardocker/templates/my_init.d/available/mount_cpuset.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
+# This script prepopulate OAR cgroup directory hierarchy, as use in the
+# job_resource_manager_cgroup.pl script, in order to have nodes use different
+# subdirectories and avoid conflitcs to to having all nodes actually running on
+# the same host machine
 
-# mkdir -p /dev/cpuset_host /dev/cpuset
-# mount -t cpuset none /dev/cpuset_host
-# mkdir "/dev/cpuset_host/$HOSTNAME"
-# mount --bind "/dev/cpuset_host/$HOSTNAME" /dev/cpuset
+ENABLE_MEMCG="NO"
+OS_CGROUPS_PATH="/sys/fs/cgroup"
+CGROUP_DIRECTORY_COLLECTION_LINKS="/dev/oar_cgroups_links"
+
+CGROUP_SUBSYSTEMS="cpuset cpu cpuacct devices freezer blkio"
+if [ "$ENABLE_MEMCG" =  "YES" ]; then
+  CGROUP_SUBSYSTEMS="cpuset cpu cpuacct devices freezer blkio memory"
+else
+  CGROUP_SUBSYSTEMS="cpuset cpu cpuacct devices freezer blkio"
+fi 
+
+mkdir -p $CGROUP_DIRECTORY_COLLECTION_LINKS && \
+for s in $CGROUP_SUBSYSTEMS; do
+  mkdir -p $OS_CGROUPS_PATH/$s/oardocker/$HOSTNAME
+  ln -s $OS_CGROUPS_PATH/$s/oardocker/$HOSTNAME $CGROUP_DIRECTORY_COLLECTION_LINKS/$s
+done
+ln -s $OS_CGROUPS_PATH/cpuset/oardocker/$HOSTNAME /dev/cpuset
+
+cat $OS_CGROUPS_PATH/cpuset/cpuset.cpus > $OS_CGROUPS_PATH/cpuset/oardocker/cpuset.cpus
+cat $OS_CGROUPS_PATH/cpuset/cpuset.mems > $OS_CGROUPS_PATH/cpuset/oardocker/cpuset.mems
+/bin/echo 0 > $OS_CGROUPS_PATH/cpuset/oardocker/cpuset.cpu_exclusive
+/bin/echo 1000 > $OS_CGROUPS_PATH/cpuset/oardocker/notify_on_release
+
+cat $OS_CGROUPS_PATH/cpuset/oardocker/cpuset.cpus > $OS_CGROUPS_PATH/cpuset/oardocker/$HOSTNAME/cpuset.cpus
+cat $OS_CGROUPS_PATH/cpuset/oardocker/cpuset.mems > $OS_CGROUPS_PATH/cpuset/oardocker/$HOSTNAME/cpuset.mems
+/bin/echo 0 > $OS_CGROUPS_PATH/cpuset/oardocker/$HOSTNAME/cpuset.cpu_exclusive
+/bin/echo 0 > $OS_CGROUPS_PATH/cpuset/oardocker/$HOSTNAME/notify_on_release
+/bin/echo 1000 > $OS_CGROUPS_PATH/blkio/oardocker/$HOSTNAME/blkio.weight
