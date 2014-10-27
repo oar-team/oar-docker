be7abbefde574251cc070c94fc35f5d12bdce70c
Salem Harrache <salem@harrache.info>
2014-10-27 20:16:36 +0100

    Added wait_pgsql script to wait postgresql to be available #14

--- oardocker/templates/my_init.d/available/init_database.sh
+++ oardocker/templates/my_init.d/available/init_database.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 set -e
 
-# Create OAR database
-echo -n "Waiting database to be available"
-until nc -z server 5432; do
-    sleep 1
-    echo -n "."
-done
-echo
+echo "Waiting postgresql to be available..."
+wait_pgsql --host server
 
 
 /usr/local/sbin/oar-database --create --db-admin-user postgres --db-admin-pass postgres --db-host server
