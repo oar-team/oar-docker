eb4e220d1c3f6dcb23746c799b804cc47579a3fd
Salem Harrache <salem@harrache.info>
2014-11-03 19:23:48 +0100

    Revert to apache2 configuration

--- oardocker/templates/my_init.d/available/start_oar_web_status.sh
+++ oardocker/templates/my_init.d/available/start_oar_web_status.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 set -e
 
-echo "Starting nginx..."
-/etc/init.d/nginx start
-echo "Starting php5-fpm..."
-/etc/init.d/php5-fpm start
-echo "Starting fcgiwrap..."
-/etc/init.d/fcgiwrap start
+echo "Starting apache..."
+/etc/init.d/apache2 start
