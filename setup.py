ee8987f31a4685e64b29b51272d9a31f25859a79
Salem Harrache <salem.harrache@inria.fr>
2015-03-31 16:55:43 +0200

    Renamed changelog

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     description='Manage a small OAR developpement cluster with docker.',
-    long_description=read('README.rst') + '\n\n' + read('CHANGELOG.rst'),
+    long_description=read('README.rst') + '\n\n' + read('CHANGES'),
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
