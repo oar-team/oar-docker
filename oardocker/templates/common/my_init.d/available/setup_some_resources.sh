ad593712d70fc115bf3785ff8d50a4ff2559d859
Salem Harrache <salem.harrache@inria.fr>
2015-05-19 18:18:36 +0200

    Fixed oar resources initialization

--- oardocker/templates/common/my_init.d/available/setup_some_resources.sh
+++ oardocker/templates/common/my_init.d/available/setup_some_resources.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
 
-STAMP="/var/lib/container/stamps_oar_resources_initialized"
+wait_pgsql --host server --user oar --password oar
 
-while [ ! -f "/var/lib/container/stamps_oar_database_created" ]
-do
-    sleep 0.1
-done
+NODE_CMD_FILE="/tmp/_oar_resources_init_current_node"
+NODE_HOST_FILE="/tmp/_oar_resources_init_hosts_file"
+while read node; do
+    wait_ssh -h $node -p 22
+    # echo $node | oar_resources_init -o -y -x
+    echo $node > "$NODE_HOST_FILE"
+    chown oar:oar "$NODE_HOST_FILE"
+    oar_resources_init "$NODE_HOST_FILE" <<< "$(echo -e 'yes\nyes\n')"
+    source /tmp/oar_resources_init.cmd
+done </var/lib/container/nodes
 
-if [ -f "$STAMP" ]; then
-    echo "OAR resources already initialized"
-else
-    DIR=`TMPDIR=/tmp mktemp -d --tmpdir ${tmpdir}_XXX`
-    NODE_NAME_FILE="$DIR/current_node"
-    NODE_CMD_FILE="$DIR/oar_resources_init.cmd"
-
-    while read node; do
-        wait_ssh -h $node -p 22
-        echo "$node" > "$NODE_FILE"
-        oar_resources_init "$NODE_FILE" -o "$NODE_CMD_FILE" <<< "$(echo -e 'yes\nyes\n')"
-        source "$NODE_CMD_FILE"
-    done </var/lib/container/nodes
-
-    touch $STAMP
-fi
+touch $STAMP
