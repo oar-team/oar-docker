87a0091e5c803e208ccddb429cc1c55899548ffc
Salem Harrache <salem.harrache@inria.fr>
2015-07-24 14:35:42 +0200

    Adapt new oar_resources_init options

--- oardocker/templates/common/my_init.d/available/setup_some_resources.sh
+++ oardocker/templates/common/my_init.d/available/setup_some_resources.sh
@@ -4,15 +4,9 @@ STAMP="/var/lib/container/stamps_oar_resources_initialized"
 
 wait_pgsql --host server --user oar --password oar
 
-NODE_CMD_FILE="/tmp/_oar_resources_init_current_node"
-NODE_HOST_FILE="/tmp/_oar_resources_init_hosts_file"
 while read node; do
     wait_ssh -h $node -p 22
-    # echo $node | oar_resources_init -o -y -x
-    echo $node > "$NODE_HOST_FILE"
-    chown oar:oar "$NODE_HOST_FILE"
-    oar_resources_init "$NODE_HOST_FILE" <<< "$(echo -e 'yes\nyes\n')"
-    source /tmp/oar_resources_init.cmd
+    echo $node | oar_resources_init -y - -x
 done </var/lib/container/nodes
 
 touch $STAMP
