33e038fc6bd209338889b1efedca9b7395c75e23
Bruno Bzeznik <Bruno.Bzeznik@imag.fr>
2015-07-28 12:26:46 +0200

    Fixed again, resources intialization

--- oardocker/templates/common/my_init.d/available/setup_some_resources.sh
+++ oardocker/templates/common/my_init.d/available/setup_some_resources.sh
@@ -8,7 +8,7 @@ NODE_CMD_FILE="/tmp/_oar_resources_init_current_node"
 NODE_HOST_FILE="/tmp/_oar_resources_init_hosts_file"
 while read node; do
     wait_ssh -h $node -p 22
-    echo $node | oar_resources_init -o -y -x
+    echo $node | oar_resources_init -y -x -
 done </var/lib/container/nodes
 
 touch $STAMP
