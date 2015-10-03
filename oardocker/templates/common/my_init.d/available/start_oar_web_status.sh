cd6d665d907365bf15a2cd3c5d92b1bb8d34fde9
Salem Harrache <salem.harrache@inria.fr>
2015-10-03 08:45:56 +0200

    Fixed perl version of API by using nginx

--- oardocker/templates/common/my_init.d/available/start_oar_web_status.sh
+++ oardocker/templates/common/my_init.d/available/start_oar_web_status.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -e
 
-echo "Starting apache..."
-/etc/init.d/apache2 restart
+echo "Starting http server..."
+/etc/init.d/nginx start
+/etc/init.d/php5-fpm start
+
+/usr/bin/spawn-fcgi -n -s /var/run/oar-fcgi.sock -u oar -g oar -U www-data -G www-data -- /usr/sbin/fcgiwrap
