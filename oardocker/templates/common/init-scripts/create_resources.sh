c52bfd1d8662765d5ca09359c04b1f0ac35fc620
Salem Harrache <salem.harrache@inria.fr>
2015-11-09 15:58:24 +0100

    Create resources manually because if faster than oar_resources_ini (no ssh connection)

--- oardocker/templates/common/init-scripts/create_resources.sh
+++ oardocker/templates/common/init-scripts/create_resources.sh
@@ -1,10 +1,44 @@
 #!/bin/bash
 
-wait_pgsql --host server --user oar --password oar
+# wait_pgsql --host server --user oar --password oar
 
-NODE_CMD_FILE="/tmp/_oar_resources_init_current_node"
-NODE_HOST_FILE="/tmp/_oar_resources_init_hosts_file"
-while read node; do
-    wait_ssh -h $node -p 22
-    echo $node | oar_resources_init -y -x -
-done </var/lib/container/nodes
+echo_and_run() { echo "$@" ; $@ ; }
+
+# slow
+# systemd-analyze blame | grep resources -> 5.469s oardocker-resources.service
+function create_resources_automatically() {
+    while read node; do
+        wait_ssh -h $node -p 22
+        echo $node | oar_resources_init -y -x -
+    done </var/lib/container/nodes
+}
+
+
+function create_resources_manually() {
+    mem=$(grep -e "^MemTotal" /proc/meminfo | awk '{print $2}')
+    mem=$((mem / 1024 / 1024 + 1))
+    num_cpuset=$(grep -e "^processor\s\+:" /proc/cpuinfo | sort -u | wc -l)
+
+    oarproperty -a cpu || true
+    oarproperty -a core || true
+    oarproperty -c -a host || true
+    oarproperty -a mem || true
+
+    cpu=1
+    while read node; do
+      index=0
+      for ((cpuset=0;cpuset<$num_cpuset; cpuset++)); do
+        echo_and_run oarnodesetting -a -h $node -p host=$node -p cpu=$cpu -p core=$((cpuset + 1)) -p cpuset=$cpuset -p mem=$mem
+      done
+      cpu=$((cpu + 1))
+    done </var/lib/container/nodes
+}
+
+
+# slower
+# systemd-analyze blame | grep resources -> 5.469s oardocker-resources.service
+# create_resources_automatically
+
+# faster
+# systemd-analyze blame | grep resources -> 2.448s oardocker-resources.service
+create_resources_manually
