6921ddcc75a6e73c6613335eb9fb30ad6ae8d340
Salem Harrache <salem.harrache@inria.fr>
2015-11-09 15:46:03 +0100

    Forward oar-server logs to stdout (journald)

---
+++ oardocker/templates/common/init-scripts/oar_server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo > /var/log/oar.log
+
+function stop_server {
+    source /etc/init.d/oar-server stop
+}
+
+trap 'stop_server' INT TERM EXIT
+
+source /etc/init.d/oar-server start
+
+tail -F -n 0 /var/log/oar.log \
+    | awk '{ $2=""; $3=""; print }' \
+    | tr -s " "
