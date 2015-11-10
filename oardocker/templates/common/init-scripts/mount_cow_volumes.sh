13c8abb316347fefc82af1116b696ff773401373
Salem Harrache <salem.harrache@inria.fr>
2015-11-10 02:13:27 +0100

    Fixed cow volumes mount script

--- oardocker/templates/common/init-scripts/mount_cow_volumes.sh
+++ oardocker/templates/common/init-scripts/mount_cow_volumes.sh
@@ -7,10 +7,12 @@ while read cow_volume; do
     # RO_PATH => the read only docker mount
     # RW_PATH => contained only local changes layer
     IFS=':' read COW_PATH RO_PATH <<< "$cow_volume"
-    mkdir -p "$COW_PATH"
-    RW_PATH=$(mktemp -d --tmpdir oardocker_mout.XXXXXXXX)
-    unionfs-fuse -o cow -o allow_other,default_permissions,use_ino,suid,dev,nonempty "$RW_PATH"=RW:"$RO_PATH"=RO "$COW_PATH"
-    chmod --reference="$RO_PATH" "$COW_PATH"
-    chown --reference="$RO_PATH" "$COW_PATH"
-    echo " -> $COW_PATH "
+    if [ -n "${COW_PATH}" ]; then
+        mkdir -p "$COW_PATH"
+        RW_PATH=$(mktemp -d --tmpdir oardocker_mout.XXXXXXXX)
+        unionfs-fuse -o cow -o allow_other,default_permissions,use_ino,suid,dev,nonempty "$RW_PATH"=RW:"$RO_PATH"=RO "$COW_PATH"
+        chmod --reference="$RO_PATH" "$COW_PATH"
+        chown --reference="$RO_PATH" "$COW_PATH"
+        echo " -> $COW_PATH "
+    fi
 done </var/lib/container/cow_volumes
