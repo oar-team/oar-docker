28b4be282b0900551d1e962edb12894383dff67c
Salem Harrache <salem@harrache.info>
2017-03-06 15:33:53 +0100

    Used the new docker-squash tool

--- scripts/build_squash_images.sh
+++ scripts/build_squash_images.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-command -v foo >/dev/null 2>&1 || { echo >&2 "Missing docker-scripts. 'pip install docker-scripts'"; exit 1; }
+command -v docker-squash >/dev/null 2>&1 || { echo >&2 "Missing docker-squash. 'pip install docker-squash'"; exit 1; }
 
 oardocker build
 for image_id in $(oardocker status | grep oardocker |  awk '{print $2}'); do
     image_tag=$( oardocker status | grep $image_id |  awk '{print $1}' )
-    docker-scripts squash $image_tag -t $image_tag
+    docker-squash $image_tag -t $image_tag
 done
 oardocker build --no-rebuild
