fcd413314a1d97fe0e316d981f6a0c174364e026
Salem Harrache <salem@harrache.info>
2017-10-08 18:02:15 +0200

    Support dev -> dev version dump

--- scripts/bumpversion.py
+++ scripts/bumpversion.py
@@ -48,8 +48,8 @@ which will create a 'release' version (Eg. 0.7.2-dev => 0.7.2).
     bumpver = subprocess.check_output(
         ['bumpversion', 'release', '--dry-run', '--verbose'],
         stderr=subprocess.STDOUT).decode('utf-8')
-    m = re.search(r'Parsing version \'(\d+\.\d+\.\d+)\.dev0\'', bumpver)
-    current_version = m.groups(0)[0] + ".dev0"
+    m = re.search(r'Parsing version \'(\d+\.\d+\.\d+)\.dev(\d+)\'', bumpver)
+    current_version = m.groups(0)[0] + ".dev" + m.groups(0)[1]
     m = re.search(r'New version will be \'(\d+\.\d+\.\d+)\'', bumpver)
     release_version = m.groups(0)[0]
 
@@ -112,10 +112,10 @@ which will create a 'major' release (0.0.2 => 1.0.0-dev)."""
     bumpver = subprocess.check_output(
         ['bumpversion', part, '--dry-run', '--verbose'],
         stderr=subprocess.STDOUT).decode("utf-8")
-    m = re.search(r'current_version.*?(\d+\.\d+\.\d+\.dev0|\d+\.\d+\.\d+)',
+    m = re.search(r'current_version.*?(\d+\.\d+\.\d+\.dev\d+|\d+\.\d+\.\d+)',
                   bumpver)
     current_version = m.groups(0)[0]
-    m = re.search(r'New version will be.*?(\d+\.\d+\.\d+\.dev0|\d+\.\d+\.\d+)',
+    m = re.search(r'New version will be.*?(\d+\.\d+\.\d+\.dev\d+|\d+\.\d+\.\d+)',
                   bumpver)
     next_version = m.groups(0)[0]
 
