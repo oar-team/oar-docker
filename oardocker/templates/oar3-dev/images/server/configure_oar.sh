d142074901436a06ec5c2ffa987b8648ecdd8f2a
Olivier Auguste Richard <olivier.richard@imag.fr>
2017-11-22 12:49:47 +0100

    Enhance oar3-dev

--- oardocker/templates/oar3-dev/images/server/configure_oar.sh
+++ oardocker/templates/oar3-dev/images/server/configure_oar.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 set -e
 
-#ln -s /usr/local/bin/kamelot /usr/local/lib/oar/schedulers/kamelot
+echo "Starting postgresql..."
+/etc/init.d/postgresql restart
 
+echo "Waiting postgresql to be available..."
+sudo -u postgres wait_pgsql
+
+echo "Enable kamelot scheduler"
+setuser postgres psql -d oar -c "update queues set scheduler_policy='kamelot';"
+
+echo "Remove admission rules #15"
+setuser postgres psql -d oar -c "delete from admission_rules where id='15';"
+
+echo "Stopping postgresql..."
+/etc/init.d/postgresql stop
+
+echo "Set some links for OAR3"
+ln -s /usr/local/bin/oar3-appendice-proxy /usr/local/lib/oar/oar3-appendice-proxy
+ln -s /usr/local/bin/oar3-bipbip-commander /usr/local/lib/oar/oar3-bipbip-commander
+
+echo "Set various things for OAR3"
+ln -s /usr/local/bin/kamelot /usr/local/lib/oar/schedulers/kamelot
 sed -i s/#HIERARCHY_LABELS/HIERARCHY_LABELS/g /etc/oar/oar.conf
-
-#echo "Starting postgresql..."
-#/etc/init.d/postgresql restart
-
-#echo "Waiting postgresql to be available..."
-#sudo -u postgres wait_pgsql
-
-#echo "Enable kamelot scheduler"
-#setuser postgres psql -d oar -c "update queues set scheduler_policy='kamelot';"
-
-#echo "Remove admission rules #15"
-#setuser postgres psql -d oar -c "delete from admission_rules where id='15';"
-
-#echo "Stopping postgresql..."
-#/etc/init.d/postgresql stop
+echo 'META_SCHED_CMD="/usr/local/bin/kao"' >> /etc/oar/oar.conf
