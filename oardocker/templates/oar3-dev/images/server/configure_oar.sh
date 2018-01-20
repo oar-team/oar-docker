d7784bc526dac6dbc46b8735dd4774b4f6f896cb
Olivier Auguste Richard <olivier.richard@imag.fr>
2018-01-20 16:23:09 +0100

    [oar3-dev] Add kao's symbolic link

--- oardocker/templates/oar3-dev/images/server/configure_oar.sh
+++ oardocker/templates/oar3-dev/images/server/configure_oar.sh
@@ -24,8 +24,9 @@ ln -s /usr/local/bin/oar3-finaud /usr/local/lib/oar/oar3-finaud
 ln -s /usr/local/bin/oar3-leon /usr/local/lib/oar/oar3-leon
 ln -s /usr/local/bin/oar3-node-change-state /usr/local/lib/oar/oar3-node-change-state
 ln -s /usr/local/bin/oar3-bipbip /usr/local/lib/oar/oar3-bipbip
+ln -s /usr/local/bin/kao /usr/local/lib/oar/kao
 
 echo "Set various things for OAR3"
 ln -s /usr/local/bin/kamelot /usr/local/lib/oar/schedulers/kamelot
 sed -i s/#HIERARCHY_LABELS/HIERARCHY_LABELS/g /etc/oar/oar.conf
-echo 'META_SCHED_CMD="/usr/local/bin/kao"' >> /etc/oar/oar.conf
+echo 'META_SCHED_CMD="/usr/local/lib/oar/kao"' >> /etc/oar/oar.conf
