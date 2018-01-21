e835a5326506657d4fe9d640c1b74c20079f2407
Olivier Auguste Richard <olivier.richard@imag.fr>
2018-01-21 22:14:49 +0100

    Add oarswitch-cli and change default oarcli to v3 if appropriate

--- oardocker/templates/oar3-dev/images/frontend/configure_oar3.sh
+++ oardocker/templates/oar3-dev/images/frontend/configure_oar3.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -e
 
-echo "Set replace some oar-v2 cli by their oar-v3 equivalent: oarsub"
-mv /usr/local/lib/oar/oarsub /usr/local/lib/oar/oarsub2
-ln -s /usr/local/bin/oarsub3 /usr/local/lib/oar/oarsub
+echo "Replace some oar-v2 cli by their oar-v3 equivalent"
+
+#TODO oarwalltime oarcp oarsh oarprint oarnodechecklist oarnodecheckquery 
+for oarcli in oarsub oarnodes oarstat oardel oarhold oarresume
+do  
+    echo "Replace $oarcli with its version 3..."
+    cmd_mv="mv /usr/local/lib/oar/$oarcli /usr/local/lib/oar/$oarcli"2
+    echo $cmd_mv
+    $cmd_mv
+    cmd_ln="ln -s /usr/local/bin/$oarcli"3" /usr/local/lib/oar/$oarcli"
+    echo $cmd_ln
+    $cmd_ln
+done  
+
--- oardocker/templates/oar3-dev/images/server/configure_oar3.sh
+++ oardocker/templates/oar3-dev/images/server/configure_oar3.sh
@@ -30,6 +30,20 @@ echo "Set oar3-almighty as Almighty"
 mv /usr/local/lib/oar/Almighty /usr/local/lib/oar/Almighty2
 ln -s /usr/local/bin/oar3-almighty /usr/local/lib/oar/Almighty
 
+echo "Replace some oar-v2 cli by their oar-v3 equivalent: oarsub"
+
+#TODO oarnodesetting oaraccounting oaradmissionrules oarnotify oar_phoenix oarprint oarproperty oarsh oarcp oar-database
+for oarcli in oarremoveresource
+do  
+    echo "Replace $oarcli with its version 3"
+    cmd_mv="mv /usr/local/lib/oar/$oarcli /usr/local/lib/oar/$oarcli"2
+    echo $cmd_mv
+    $cmd_mv
+    cmd_ln="ln -s /usr/local/bin/$oarcli"3" /usr/local/lib/oar/$oarcli"
+    echo $cmd_ln
+    $cmd_ln
+done  
+
 echo "Set various things for OAR3"
 ln -s /usr/local/bin/kamelot /usr/local/lib/oar/schedulers/kamelot
 sed -i s/#HIERARCHY_LABELS/HIERARCHY_LABELS/g /etc/oar/oar.conf
