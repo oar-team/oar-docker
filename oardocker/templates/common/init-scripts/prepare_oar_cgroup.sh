63f8f16f9bc3cbed6564fde52c841f9c7f6d8913
Pierre Neyron <pierre.neyron@imag.fr>
2020-03-29 23:02:50 +0200

    cgroup setup: let oar create the oar_cgroups_links, but handle all subsystems [breaks oar < 2.5.9+g5k5]
    The job_resource_manager has to be aware of oardocker to link to
    correctly to the <cgsubsystem>/oardocker/<nodename> subdirectories.

--- oardocker/templates/common/init-scripts/prepare_oar_cgroup.sh
+++ oardocker/templates/common/init-scripts/prepare_oar_cgroup.sh
@@ -5,26 +5,16 @@
 # the same host machine
 
 OS_CGROUPS_PATH="/sys/fs/cgroup"
-CGROUP_SUBSYSTEMS="cpuset cpu cpuacct devices freezer blkio"
-if [ -e "$OS_CGROUPS_PATH/memory" ]; then
-  CGROUP_SUBSYSTEMS="$CGROUP_SUBSYSTEMS memory"
-fi
-CGROUP_DIRECTORY_COLLECTION_LINKS="/dev/oar_cgroups_links"
-
 
 if [ "$1" = "init" ]; then
-    mkdir -p $CGROUP_DIRECTORY_COLLECTION_LINKS && \
-    for s in $CGROUP_SUBSYSTEMS; do
-      mkdir -p $OS_CGROUPS_PATH/$s/oardocker/$HOSTNAME
-      ln -s $OS_CGROUPS_PATH/$s/oardocker/$HOSTNAME $CGROUP_DIRECTORY_COLLECTION_LINKS/$s
+    for cg in $OS_CGROUPS_PATH/*; do
+        [ -d $cg ] && mkdir -p $cg/oardocker/$HOSTNAME
     done
-    ln -s $OS_CGROUPS_PATH/cpuset/oardocker/$HOSTNAME /dev/cpuset
-
     cat $OS_CGROUPS_PATH/cpuset/cpuset.cpus > $OS_CGROUPS_PATH/cpuset/oardocker/cpuset.cpus
     cat $OS_CGROUPS_PATH/cpuset/cpuset.mems > $OS_CGROUPS_PATH/cpuset/oardocker/cpuset.mems
     /bin/echo 0 > $OS_CGROUPS_PATH/cpuset/oardocker/cpuset.cpu_exclusive
-    /bin/echo 1000 > $OS_CGROUPS_PATH/cpuset/oardocker/notify_on_release
-
+    /bin/echo 0 > $OS_CGROUPS_PATH/cpuset/oardocker/notify_on_release
+    /bin/echo 1000 > $OS_CGROUPS_PATH/blkio/oardocker/blkio.weight
     cat $OS_CGROUPS_PATH/cpuset/oardocker/cpuset.cpus > $OS_CGROUPS_PATH/cpuset/oardocker/$HOSTNAME/cpuset.cpus
     cat $OS_CGROUPS_PATH/cpuset/oardocker/cpuset.mems > $OS_CGROUPS_PATH/cpuset/oardocker/$HOSTNAME/cpuset.mems
     /bin/echo 0 > $OS_CGROUPS_PATH/cpuset/oardocker/$HOSTNAME/cpuset.cpu_exclusive
@@ -32,23 +22,16 @@ if [ "$1" = "init" ]; then
     /bin/echo 1000 > $OS_CGROUPS_PATH/blkio/oardocker/$HOSTNAME/blkio.weight
 elif [ "$1" = "clean" ]; then
     if [ "$HOSTNAME" = "node1" ]; then
-        CGROOT="$OS_CGROUPS_PATH/cpuset/oardocker/"
-
-        if ! [ -d $CGROOT ]; then
-          echo "No such directory: $CGROOT"
-          exit 0;
-        fi
-
-        echo "kill all cgroup tasks"
-        while read task; do
-            echo "kill -9 $task"
-            kill -9 $task
-        done < <(find $CGROOT -name tasks -exec cat {} \;)
-
-        wait
-        echo "Wipe all cgroup content"
-        find $CGROOT -depth -type d -exec rmdir {} \;
-
+        for cg in $OS_CGROUPS_PATH/cpuset/oardocker $OS_CGROUPS_PATH/*/oardocker; do
+            echo "kill all cgroup tasks"
+            while read task; do
+                echo "kill -9 $task"
+                kill -9 $task
+            done < <(find $cg -name tasks -exec cat {} \;)
+            wait
+            echo "Wipe all cgroup content"
+            find $cg -depth -type d -exec rmdir {} \;
+        done
         echo "Cgroup is cleanded!"
     fi
 fi
